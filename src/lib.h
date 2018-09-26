853df73b244ac568e770065e429356b71f138608
guillaume <gsavreux92@gmail.com>
2018-09-26 12:22:54 +0200

    doxygen

--- src/lib.h
+++ src/lib.h
@@ -1,13 +1,43 @@
+/*!\file lib.h
+*\brief description
+*\author prenom
+*\version
+*/
+
 #pragma once
 
 class calcul
+
+
+/*!\class calcul
+*\brief
+*
+*
+*
+*/
+
+
 {
        public:
 
+/*!\brief
+*\param a description...
+*\param b description...
+*\return
+*/
 
 
+        
               int somme (int nombre_1, int nombre_2);
+/*!\brief
+*\param c description...
+*\param d description...
+*\return
+*/
+
               int multiplication (int nombre_3, int nombre_4);
+
+
               int division (int nombre_5, int nombre_6);
 
 
